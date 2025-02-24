3365c5b0e79e9262fadae445fa85ebd462add13b
JNo <jnoelgeorge@gmail.com>
2025-02-24 14:33:57 +1100

    Update S2-MT-Qualtrics-IAT.js

--- S2-MT-Qualtrics-IAT.js
+++ S2-MT-Qualtrics-IAT.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/whoisjno/qualtrics-IAT@main/S2-IAT-Minno_Script.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
