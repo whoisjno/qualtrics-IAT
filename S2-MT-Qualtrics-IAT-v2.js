4c20dc7401b45f9e73dd5af05772629c36d410b7
JNo <jnoelgeorge@gmail.com>
2025-02-27 11:08:26 +1100

    Update S2-MT-Qualtrics-IAT-v3.js

--- S2-MT-Qualtrics-IAT-v2.js
+++ S2-MT-Qualtrics-IAT-v2.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/whoisjno/qualtrics-IAT@main/S2-IAT-Minno_Script-Pilot.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualtrics/quiat9.js'], function(APIConstructor, iatExtension){
 	var API = new APIConstructor();
 
 	return iatExtension({
@@ -45,6 +45,29 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/whoisjno/qualtrics-IAT@main/S2-IA
     			{image : '2044.jpg'}			],
 			//Stimulus css
 			stimulusCss : {color:'#31940F','font-size':'1.8em'}
+		},
+		attribute1 :
+		{
+			name : 'Bad words',
+			title : {
+				media : {word : 'Bad words'},
+				css : {color:'#0000FF','font-size':'1.8em'},
+				height : 4 //Used to position the "Or" in the combined block.
+			},
+			stimulusMedia : [ //Stimuli content as PIP's media objects
+				{word: 'dangerous'},
+				{word: 'unreliable'},
+				{word: 'dreadful'},
+				{word: 'terrible'},
+				{word: 'nasty'},
+				{word: 'evil'},
+				{word: 'horrible'},
+				{word: 'dead'},
+				{word: 'pain'},
+				{word: 'failure'}
+			],
+			//Stimulus css
+			stimulusCss : {color:'#0000FF','font-size':'2.3em'}
 		},
 
 		base_url : {//Where are your images at?
